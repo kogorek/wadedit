a21fd5983bb1b1f96a48d12715792b2221559ac1
moksou <makron2145@gmail.com>
2016-03-05 09:20:33 +0300

    test

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "lump.c"
 
 FILE *fp;
 
@@ -8,10 +9,14 @@ int numFiles, dirStart;
 int offset, size;
 char lumpname[9];
 
+const char* lump_getNameIndex(FILE*, int);
+int * lump_getSize(FILE*, int);
+
 int main(int argc, char *argv[])
 {
-    if(argc < 2){
-        puts("Usage: ./wadedit filename.wad [sta from]");
+
+    if(argc != 2){
+        puts("Usage: ./wadedit filename.wad");
         return 1;
     }
 
@@ -27,16 +32,41 @@ int main(int argc, char *argv[])
     fread(&numFiles, 4, 1, fp);
     fread(&dirStart, 4, 1, fp);
     printf("Opened %s with %d lumps.\n", signature, numFiles);
-    fseek(fp, dirStart, SEEK_SET);
-
-    puts(" Num    Offset      Size  Filename");
-    puts("----------------------------------");
-
-    for(int i = 1; i <= numFiles; i++){
-      if(fread(&offset, 4, 1, fp) && fread(&size, 4, 1, fp) && fread(&lumpname, 8, 1, fp)){
-            printf("%4d %9d %9d %9s \n", i, offset, size, lumpname);
-      }
+    for(int i = 0; i < numFiles; i++){
+        printf("%9s   %9d\n", lump_getNameIndex(fp, i+1), lump_getSize(fp, i+1));
     }
     fclose(fp);
+    return 0;
+}
+
+const char* lump_getNameIndex(FILE* fp, int num)
+{
+    if(num == 0){
+        puts("bad input");
+        return 1;
+    }
+    static char name[9] = {0};
+
+    fseek(fp, dirStart + 8, SEEK_SET);
+    for(int i = 1; i <= numFiles; i++){
+        if(i == num){
+            fread(&name, 8, 1, fp);
+            return name;
+        }
+        fseek(fp, 16, SEEK_CUR);
+    }
+    return 0;
+}
+
+int* lump_getSize(FILE* fp, int index){
+    static int size;
+    fseek(fp, dirStart + 4, SEEK_SET);
+    for(int i = 1; i <= numFiles; i++){
+        if(i == index){
+            fread(&size, 4, 1, fp);
+            return size;
+        }
+        fseek(fp, 16, SEEK_CUR);
+    }
     return 0;
 }
